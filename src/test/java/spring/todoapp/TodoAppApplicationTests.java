35dabf9bcbf94a573bb078768e2e9ca1ef12c38d
Malktom <tomasz.malkiewicz@yahoo.com>
2022-11-20 18:59:43 +0100

    Initial commit

---
+++ src/test/java/spring/todoapp/TodoAppApplicationTests.java
@@ -0,0 +1,16 @@
+package spring.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+
+	}
+
+
+}
